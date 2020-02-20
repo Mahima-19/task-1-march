9717ed63b3063f3b85075b18a5eaaaaa0750781b
rajan.opallabs <rajan.opallabs@gmail.com>
2020-02-20 14:18:51 +0530

    Intent

--- layouts/src/main/java/com/codekul/layouts/LoginActivity.java
+++ layouts/src/main/java/com/codekul/layouts/LoginActivity.java
@@ -2,13 +2,36 @@ package com.codekul.layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsername,edtPassword;
+    TextView signUp;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    edtUsername=findViewById(R.id.edtUsername);
+    edtPassword=findViewById(R.id.edtPassword);
+    signUp=findViewById(R.id.txtSignUp);
+    btnLogin=findViewById(R.id.btnLogIn);
+
+    signUp.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(LoginActivity.this,RegistrationActivity.class);
+            startActivity(intent);
+        }
+    });
+
     }
 }
